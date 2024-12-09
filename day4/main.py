cf6b6ef4741368ff5e41fd1874809357b1e7f3ef
sherySSH <hussainsyedshaharyaar@gmail.com>
2024-12-09 22:16:11 +0500

    added top and bottom search for d4p1

--- day4/main.py
+++ day4/main.py
@@ -42,6 +42,37 @@ def search_left(grid : list, position : tuple, expected_word : str, searched_wor
         return search_left(grid, position, expected_word, searched_word)
     else:
         return 0
+    
+
+def search_top(grid : list, position : tuple, expected_word : str, searched_word : str):
+
+    searched_letter = grid[position.y][position.x]
+    expected_letter = expected_word[len(searched_word)]
+    searched_word = searched_word + searched_letter
+
+    position.y = position.y - 1
+
+    if searched_letter == expected_letter and expected_word == searched_word:
+        return 1
+    elif searched_letter == expected_letter and expected_word != searched_word:
+        return search_top(grid, position, expected_word, searched_word)
+    else:
+        return 0
+    
+def search_bottom(grid : list, position : tuple, expected_word : str, searched_word : str):
+
+    searched_letter = grid[position.y][position.x]
+    expected_letter = expected_word[len(searched_word)]
+    searched_word = searched_word + searched_letter
+
+    position.y = position.y + 1
+
+    if searched_letter == expected_letter and expected_word == searched_word:
+        return 1
+    elif searched_letter == expected_letter and expected_word != searched_word:
+        return search_bottom(grid, position, expected_word, searched_word)
+    else:
+        return 0
 
 
 if __name__ == "__main__":
@@ -50,4 +81,8 @@ if __name__ == "__main__":
     result = search_right(grid, Position(x=6, y=0), 'XMAS', '')
     print(result)
     result = search_left(grid, Position(x=43, y=0), 'XMAS', '')
+    print(result)
+    result = search_top(grid, Position(x=30, y=138), 'XMAS', '')
+    print(result)
+    result = search_bottom(grid, Position(x=76, y=117), 'XMAS', '')
     print(result)
