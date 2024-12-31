695ac7ea79058e29e486e2677fba92962f509d95
sherySSH <hussainsyedshaharyaar@gmail.com>
2024-12-31 19:02:09 +0500

    added day6 part 2

--- day10/partb.py
+++ day10/partb.py
@@ -11,7 +11,6 @@ class Position:
 @dataclass
 class Node:
     value : int
-    visited : bool
 
 def read_file(path : str) -> List[str]:
     with open(path, 'r') as f:
@@ -24,7 +23,7 @@ def processing(content : str):
 
     for y in range(len(grid)):
         for x in range(len(grid[y])):
-            grid[y][x] = Node( value = int(grid[y][x]) , visited = False)
+            grid[y][x] = Node( value = int(grid[y][x]) )
 
     return grid
 
@@ -42,14 +41,13 @@ def search_top(grid : List[List[Node]] , current_position : Position):
 
     current_height = grid[current_position.y][current_position.x].value
     next_position : Position = None
-    # checking grid bound
-    if (current_position.y - 1) >= 0 and not grid[current_position.y - 1][current_position.x].visited:
+    # checking grid bound for next position
+    if (current_position.y - 1) >= 0:
         next_height = grid[current_position.y - 1][current_position.x].value
         slope = next_height - current_height
 
         if slope == 1:
             next_position = Position(x=current_position.x,y=current_position.y-1)
-            grid[current_position.y - 1][current_position.x].visited = True
     
     return next_position
 
@@ -59,14 +57,14 @@ def search_right(grid : List[List[Node]] , current_position : Position):
     
     current_height = grid[current_position.y][current_position.x].value
     next_position : Position = None
-    # checking grid bound
-    if (current_position.x + 1) < len(grid[current_position.y]) and not grid[current_position.y][current_position.x + 1].visited:
+    # checking grid bound for next position
+    if (current_position.x + 1) < len(grid[current_position.y]):
         next_height = grid[current_position.y][current_position.x + 1].value
         slope = next_height - current_height
 
         if slope == 1:
-            next_position = Position(x=current_position.x + 1, y=current_position.y)
-            grid[current_position.y][current_position.x + 1].visited = True
+            next_position = Position(x=current_position.x + 1,y=current_position.y)
+
     
     return next_position
 
@@ -75,14 +73,13 @@ def search_bottom(grid : List[List[Node]] , current_position : Position):
 
     current_height = grid[current_position.y][current_position.x].value
     next_position : Position = None
-    # checking grid bound
-    if (current_position.y + 1) < len(grid) and not grid[current_position.y + 1][current_position.x].visited:
+    # checking grid bound for next position
+    if (current_position.y + 1) < len(grid):
         next_height = grid[current_position.y + 1][current_position.x].value
         slope = next_height - current_height
 
         if slope == 1:
-            next_position = Position(x=current_position.x , y=current_position.y + 1)
-            grid[current_position.y + 1][current_position.x].visited = True
+            next_position = Position(x=current_position.x,y=current_position.y + 1)
     
     return next_position
 
@@ -91,21 +88,18 @@ def search_left(grid : List[List[Node]] , current_position : Position):
     
     current_height = grid[current_position.y][current_position.x].value
     next_position : Position = None
-    # checking grid bound
-    if (current_position.x - 1) >= 0 and not grid[current_position.y][current_position.x - 1].visited:
+    # checking grid bound for next position
+    if (current_position.x - 1) >= 0:
         next_height = grid[current_position.y][current_position.x - 1].value
         slope = next_height - current_height
         
         if slope == 1:
-            next_position = Position(x=current_position.x - 1, y=current_position.y)
-            grid[current_position.y][current_position.x - 1].visited = True
+            next_position = Position(x=current_position.x - 1,y=current_position.y)
     
     return next_position
 
 
-
-
-def search_grid(grid : List[List[Node]], trail : List[Position], trailhead_score=0):
+def search_grid(grid : List[List[Node]], trail : List[Position], trailhead_score=0, ):
     
     # base case
     if len(trail) == 0:
@@ -113,8 +107,11 @@ def search_grid(grid : List[List[Node]], trail : List[Position], trailhead_score
     
     # recursive case
     else:
+        # we keep poping the positions from trail/path stack to explore the Nodes in DFS fashion
+        # 9 height node is considered a leaf therefore it is not explored
+        # any node with slope > 1 is also considered a leaf and it is not explored
+        # any node with slope = 1 with height not equal to 9 is considered a interal node and hence it is explored
         current_position = trail.pop()
-        grid[current_position.y][current_position.x].visited = True
 
         top_pos = search_top(grid, current_position )
         right_pos = search_right(grid, current_position)
@@ -141,7 +138,14 @@ def search_grid(grid : List[List[Node]], trail : List[Position], trailhead_score
         # internal node case
         elif left_pos is not None: trail.append(left_pos)
 
-        return search_grid(grid, trail, trailhead_score)
+
+
+        # once we peform recursion then we move to new current position 
+        return search_grid(
+                           grid,
+                           trail,
+                           trailhead_score=trailhead_score
+                        )
         
 
 def search_over_all_trailheads(grid : List[List[Node]], trailheads : List[Position]):
@@ -158,4 +162,4 @@ if __name__ == "__main__":
     grid = processing(content)
     trailheads = get_trailheads(grid)
     sum = search_over_all_trailheads(grid, trailheads)
-    print("Part a:", sum)
+    print("Part b:", sum)
