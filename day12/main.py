d6a434f6e5baeb218c383f7088537cc350b48a96
sherySSH <hussainsyedshaharyaar@gmail.com>
2025-01-14 18:31:45 +0500

    added docstring for day 12 part 1

--- day12/main.py
+++ day12/main.py
@@ -119,6 +119,29 @@ def mark_region(grid : List[List[Node]], queue_p1 : list, queue_p2 : list, regio
 
     Priority importance is:
     1 has higher priority than 2
+
+    This is a priority algorithm, because it first prioritizes to find the 
+    same-valued region of the current node, and only once it had found one
+    region of same-valued nodes it will proceed to check the region of differently-valued node
+    It does not find all regions together, instead it finds the regions one-by-one.
+
+    Finding 1 region is 1 solution, once it had found 1 solution. Then it proceeds to find other
+    region, hence other solution. When all solutions have been found then we can use colored
+    nodes to compute the area of region.
+
+    During BFS traverasal, all neighbouring nodes whose value is different from current node
+    are placed in queue_p2 (2nd Priority Queue) meanwhile those neighbouring nodes that have
+    same valued are placed in queue_p1 (1st Priority Queue). When queue_p1 is empty then it
+    means we have discovered 1 region and no nodes are further remain to be explored in that
+    region hence that region is concluded/found. Then we pop the node from queue_p2 and place
+    in queue_p1 to discover the region of that node.
+
+    Example to Dry Run the implemented algorithm:
+
+    R   R   R   I   I
+    V   R   R   V   I
+    V   R   I   I   I
+    I   O   O   R   R
     """
     
     
@@ -143,10 +166,12 @@ def mark_region(grid : List[List[Node]], queue_p1 : list, queue_p2 : list, regio
             new_queue_p1, queue_p2 = update_queue_for_area(grid, current_pos, new_queue_p1, queue_p2, child_pos)
 
         if len(new_queue_p1) == 0 and len(queue_p2) != 0:
+            # keep popping nodes from queue_p2 until an uncolored node is found
+            # whose region is still needs to be discovered
             current_pos : Position = queue_p2.pop(0)
             while grid[current_pos.y][current_pos.x].region != None and len(queue_p2) != 0:
                 current_pos : Position = queue_p2.pop(0)
-
+            
             if len(queue_p2) !=0: new_queue_p1.append( current_pos )
             # now next region would be different
             # in other words, color of upcoming nodes will be different
